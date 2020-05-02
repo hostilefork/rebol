8d82e61e70f4f963ac212c7b961f8d504b963110
Brian Dickens <hostilefork@gmail.com>
2020-05-02 17:14:57 -0400

    Fix use of EVAL_MASK_DEFAULT over FEED_MASK_DEFAULT
    The bit patterns for EVAL and feeds are different.  In GET_XXX_FLAG()
    and SET_XXX_FLAG() errors are reduced by making those macros do
    token pasting.  But that doesn't catch some cases.
    
    Since it was caught by an experimental use of NODE_FLAG_4 to
    indicate REBFRM* "nodes", that is kept in this commit for the moment.

--- src/include/sys-rebnod.h
+++ src/include/sys-rebnod.h
@@ -422,11 +422,17 @@ union Reb_Header {
 #define NODE_BYTEMASK_0x10_MARKED 0x10
 
 
-//=//// NODE_FLAG_4 (fifth-leftmost bit) //////////////////////////////////=//
+//=//// NODE_FLAG_FRAME (fifth-leftmost bit) //////////////////////////////=//
 //
-#define NODE_FLAG_4 \
+// !!! The idea of being able to tell the difference between a REBFRM* by
+// looking at the first byte is appealing (avoiding strict alias).  But there
+// are so few node bits it might not be a good idea.  At least temporarily,
+// EVAL_MASK_DEFAULT sets this bit and it assists to tell when feed flags
+// and eval flags are confused.
+//
+#define NODE_FLAG_FRAME \
     FLAG_LEFT_BIT(4)
-#define NODE_BYTEMASK_0x08_4 0x08
+#define NODE_BYTEMASK_0x08_FRAME 0x08
 
 
 //=//// NODE_FLAG_ROOT (sixth-leftmost bit) ///////////////////////////////=//

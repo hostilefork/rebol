688898b58449b49bf420c644817ae8de0e51e046
Brian Dickens <hostilefork@gmail.com>
2021-01-22 03:48:16 -0500

    Stricter rules for virtual bind parameters
    Virtual binding cache information can't be run on REB_QUOTED cells,
    because the cache slot is the same one that QUOTED! uses to store its
    payload.  But this wasn't being enforced, so there were bugs when
    dealing with caching at higher quote levels (though they would just
    manifest as performance bugs, most likely).

--- src/include/sys-bind.h
+++ src/include/sys-bind.h
@@ -596,19 +596,20 @@ inline static option(REBCTX*) Get_Word_Context(
     // matches the cache in the word, then trust the information in it...
     // whether that's a hit or a miss.
     //
-    if (specifier == VAL_WORD_CACHE(any_word)) {
+    REBCEL(const*) cell = VAL_UNESCAPED(any_word);
+    if (specifier == VAL_WORD_CACHE(cell)) {
         //
         // Since the number of bits available in a virtual bind is limited,
         // the value stored is the index modulo MONDEX_MOD.  A miss is
         // recorded with the actual value MONDEX_MOD (since 0 can be an
         // actual modulus result).
         //
-        REBLEN mondex = VAL_WORD_VIRTUAL_MONDEX_UNCHECKED(any_word);
+        REBLEN mondex = VAL_WORD_VIRTUAL_MONDEX_UNCHECKED(cell);
         if (mondex == MONDEX_MOD)
             goto virtual_miss;
 
       blockscope {
-        const REBCAN *canon = VAL_WORD_CANON(VAL_UNESCAPED(any_word));
+        const REBCAN *canon = VAL_WORD_CANON(cell);
 
         // We have the primary binding's spelling to check against, so we
         // can recognize when the lossy index matches up.  It needs to match
@@ -622,7 +623,7 @@ inline static option(REBCTX*) Get_Word_Context(
             assert(GET_ARRAY_FLAG(specifier, IS_PATCH));
             if (
                 IS_SET_WORD(ARR_SINGLE(specifier))
-                and REB_SET_WORD != CELL_KIND(VAL_UNESCAPED(any_word))
+                and REB_SET_WORD != CELL_KIND(cell)
             ){
                 goto skip_hit_patch;
             }
@@ -664,9 +665,9 @@ inline static option(REBCTX*) Get_Word_Context(
         // this word is overridden without doing a linear search.  Do it
         // and then save the hit or miss information in the word for next use.
         //
-        INIT_VAL_WORD_CACHE(any_word, specifier);  // we're updating it
+        INIT_VAL_WORD_CACHE(cell, specifier);  // we're updating it
 
-        const REBCAN *canon = VAL_WORD_CANON(VAL_UNESCAPED(any_word));
+        const REBCAN *canon = VAL_WORD_CANON(cell);
 
         // !!! Virtual binding could use the bind table as a kind of next
         // level cache if it encounters a large enough object to make it
@@ -675,7 +676,7 @@ inline static option(REBCTX*) Get_Word_Context(
         do {
             if (
                 IS_SET_WORD(ARR_SINGLE(specifier))
-                and REB_SET_WORD != CELL_KIND(VAL_UNESCAPED(any_word))
+                and REB_SET_WORD != CELL_KIND(cell)
             ){
                 goto skip_miss_patch;
             }
@@ -707,7 +708,7 @@ inline static option(REBCTX*) Get_Word_Context(
                 // since specifier chains change frames for relativization,
                 // we have to store the head of the chain.  Review.
                 //
-                INIT_VAL_WORD_VIRTUAL_MONDEX(any_word, index % MONDEX_MOD);
+                INIT_VAL_WORD_VIRTUAL_MONDEX(cell, index % MONDEX_MOD);
                 *index_out = index;
                 return overload;
             }
@@ -720,7 +721,7 @@ inline static option(REBCTX*) Get_Word_Context(
 
         // Update the cache to say we miss on this particular specifier
         //
-        INIT_VAL_WORD_VIRTUAL_MONDEX(any_word, MONDEX_MOD);
+        INIT_VAL_WORD_VIRTUAL_MONDEX(cell, MONDEX_MOD);
 
         // The linked list of specifiers bottoms out with either null or the
         // varlist of the frame we want to bind relative values with.  So
@@ -962,8 +963,9 @@ inline static REBVAL *Derelativize(
         // We don't want to do this with REB_QUOTED since the cache is shared.
         //
         if (KIND3Q_BYTE_UNCHECKED(v) != REB_QUOTED) {
-            INIT_VAL_WORD_CACHE(out, UNSPECIFIED);
-            INIT_VAL_WORD_VIRTUAL_MONDEX(out, MONDEX_MOD);  // necessary?
+            REBCEL(const*) out_cell = cast(REBCEL(const*), out);
+            INIT_VAL_WORD_CACHE(out_cell, UNSPECIFIED);
+            INIT_VAL_WORD_VIRTUAL_MONDEX(out_cell, MONDEX_MOD);  // necessary?
         }
         return cast(REBVAL*, out);
     }
